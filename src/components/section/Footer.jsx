f7c2a5f4da7071d1b8b8580737cdee9cd46ac1f2
d0nghee <hdh960326@gmail.com>
2025-10-24 16:42:48 +0900

    12번째 수정

---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id='footer' role='contentinfo'>
+      <a href="mailto:imnhwoo@gmail.com" rel='noopenner noreferrer'>
+        imnhwoo@gmail.com
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
