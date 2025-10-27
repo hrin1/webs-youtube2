a7a85b9401e82cea215af552f7e0e4ebe8708391
d0nghee <hdh960326@gmail.com>
2025-10-27 09:31:03 +0900

    14번째 작업

--- src/components/contents/Website.jsx
+++ src/components/contents/Website.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import {websiteText} from '../../data/website'
+import {Link} from 'react-router-dom'
 
 const Website = () => {
   return (
-    <div>Website</div>
-  )
-}
+    <section id="website">
+      <h2>😄 웹사이트의 기초는 이 강의로~</h2>
+      <div className="video__inner">
+        {websiteText.map((video, key) => (
+          <div className="video" key={key}>
+            <div className="video__thumb play__icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Website
+export default Website;
